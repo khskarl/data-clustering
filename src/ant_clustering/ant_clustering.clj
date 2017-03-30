860b6c939a67ac55ccf0add34ce065a3cbef0253
Eu <karllhenning64@gmail.com>
2017-03-29 23:21:34 -0300

    Formatted some code

--- src/ant_clustering/ant_clustering.clj
+++ src/ant_clustering/ant_clustering.clj
@@ -18,24 +18,19 @@
 
 (def dimension 50)
 
-(defn wrap
-  [x]
+(defn wrap [x]
   (mod x dimension))
 
-(defn get-tile-ref
-  [grid [i j]]
+(defn get-tile-ref [grid [i j]]
   (get-in grid [i j]))
 
-(defn is-tile-free?
-  [grid [i j]]
+(defn is-tile-free? [grid [i j]]
   (= false (:is-busy (deref (get-tile-ref grid [i j])))))
 
-(defn is-tile-busy?
-  [grid [i j]]
+(defn is-tile-busy? [grid [i j]]
   (:is-busy (deref (get-tile-ref grid [i j]))))
 
-(defn make-grid
-  [dimension f]
+(defn make-grid [dimension f]
   (apply vector 
          (map (fn [_] 
                 (apply vector (map (fn [_] (f)) 
@@ -47,11 +42,8 @@
 (def dead-grid  (make-grid dimension #(ref (struct tile-struct false nil))))
 (def alive-grid (make-grid dimension #(ref (struct tile-struct false nil))))
 
-(defn get-body-ref-in-pos
-  [[i j]]
-  ;; (println "GETTING BODY :D")
+(defn get-body-ref-in-pos [[i j]]
   (:occupied-by (deref (get-tile-ref dead-grid [i j]))))
-
 
 ;; Ants
 (defstruct ant-struct :x :y :carrying)
@@ -60,11 +52,10 @@
   [ant]
   (not (nil? (:carrying ant))))
 
-
 (defn create-ant
   [[i j]]
   (let [tile-ref (get-tile-ref alive-grid [i j])
-        tile     (deref tile-ref)
+        tile     @tile-ref
         ant-ref (ref (struct ant-struct j i nil))] 
     (ref-set tile-ref (-> tile
                           (assoc :is-busy true)
@@ -92,10 +83,12 @@
 (def ants (create-ants num-ants))
 
 ;; Bodies
+(defstruct body-struct :x :y)
+
 (defn create-body
   [[i j]]
   (let [tile-ref (get-tile-ref dead-grid [i j])
-        tile     (deref tile-ref)
+        tile     @tile-ref
         body-ref (ref (struct ant-struct j i))] 
     (ref-set tile-ref (-> tile
                           (assoc :is-busy true)
@@ -119,8 +112,7 @@
 (def num-bodies 450)
 (def bodies (create-bodies num-bodies))
 
-(defn has-body-below-ant?
-  [ant]
+(defn has-body-below-ant? [ant]
   (let [i (:y ant)
         j (:x ant)] 
     (is-tile-busy? dead-grid [i j])))
@@ -159,17 +151,17 @@
 (defn move-ant!
   [ant [dx dy]]
   (dosync
-   (let [x (:x (deref ant))
-         y (:y (deref ant))
+   (let [x (:x @ant)
+         y (:y @ant)
          new-x (wrap (+ x dx))
          new-y (wrap (+ y dy))]
      (if (is-tile-free? alive-grid [new-y new-x])
        (let [tile-ref (get-tile-ref alive-grid [y x])
-             tile     (deref tile-ref)
+             tile     @tile-ref
              next-tile-ref (get-tile-ref alive-grid [new-y new-x])
-             next-tile     (deref next-tile-ref)]
-         (ref-set tile-ref      (assoc tile      :is-busy false))
-         (ref-set next-tile-ref (assoc next-tile :is-busy true))
+             next-tile     @next-tile-ref]
+         (alter tile-ref      assoc :is-busy false)
+         (alter next-tile-ref assoc :is-busy true)
          (alter ant assoc :x new-x)
          (alter ant assoc :y new-y))
        ant))))
@@ -190,15 +182,15 @@
 (defn pick-body-below!
   ""
   [ant-ref]
-  (let [i (:y (deref ant-ref))
-        j (:x (deref ant-ref))
+  (let [i (:y @ant-ref)
+        j (:x @ant-ref)
         body-below (get-body-ref-in-pos [i j])]
     (pick-body! ant-ref body-below)))
 
 (defn drop-body-below!
   [ant-ref]
-  (let [i (:y (deref ant-ref))
-        j (:x (deref ant-ref))
+  (let [i (:y @ant-ref)
+        j (:x @ant-ref)
         tile-ref (get-tile-ref dead-grid [i j])
         body-ref (:carrying (deref ant-ref))]
     (dosync
@@ -208,15 +200,6 @@
      (alter body-ref assoc :y i)
      (alter ant-ref assoc :carrying nil))))
 
-
-;;if ((t/=d/2) < 1) return ((c/2)*(t*t)) + b;
-;;return -c/2 * (((t-2)*(--t)) - 1) + b;
-
-(defn ease-in-out
-  [x]
-  (+ 0.5 (* (/ -1 2) (- (* (- x 2) (- x 1)) 1))) 
-  )
-
 (defn iterate-ant
   [ant-ref]
   (let [i (:y @ant-ref)
@@ -224,9 +207,6 @@
         has-body-below (has-body-below-ant? @ant-ref)
         is-carrying (is-ant-carrying? @ant-ref)
         chance (rand)]
-    ;; (println @ant-ref)
-    ;; (println "drop" (chance-to-drop @ant-ref))
-    ;; (println "pick" (chance-to-pick @ant-ref))
     (if (and is-carrying
              (not has-body-below)
              (>= (chance-to-drop @ant-ref) chance)) 
@@ -239,8 +219,6 @@
   ;; (move-ant! ant-ref [0 1])
   )
 
-(defn iterate-system
-  []
-  (run! iterate-ant ants)
-  )
+(defn iterate-system []
+  (run! iterate-ant ants))
 
